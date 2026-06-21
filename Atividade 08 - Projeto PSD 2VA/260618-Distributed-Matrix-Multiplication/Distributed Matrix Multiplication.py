c218dac498ff1db2d37300d279b8bcd8ec6cab62
Norman Santos <normanwhittlecliff@gmail.com>
2026-06-21 14:43:43 -0300

    Add files via upload

--- Atividade 08 - Projeto PSD 2VA/260618-Distributed-Matrix-Multiplication/Distributed Matrix Multiplication.py
+++ Atividade 08 - Projeto PSD 2VA/260618-Distributed-Matrix-Multiplication/Distributed Matrix Multiplication.py
@@ -8,19 +8,19 @@ import os
 import sys
 import time
 
-# ==========
+# ====================
 # CONSTANTS
 
 DEFAULT_PORT = 5000
 
-# ==========
+# ====================
 # USABLE FUNCTIONS
 
 def exitIfNone(object):
     if object is None or object == "":
         sys.exit(0)
 
-# ==========
+# ====================
 # P1-P5 FUNCTIONS
 
 def workerProcess(matrixA, matrixB, start, end, queue):
@@ -34,30 +34,38 @@ def runParallel(matrixA, matrixB, numberOfProcesses):
     processes = []
     rows = getRowLength(matrixA)
     chunk = rows // numberOfProcesses  # How many times we be dividing the work of multiplying the matrices
-    # ----------
+    # --------------------
+    startTime = time.time()         # TIME STARTED
     for workerIndex in range(numberOfProcesses):
         startRow = workerIndex * chunk
-         # ----------
+         # --------------------
         if workerIndex == numberOfProcesses - 1:
             endRow = getRowLength(matrixA)
         else:
             endRow = startRow + chunk
-         # ----------
+         # --------------------
         process = multiprocessing.Process(target=workerProcess, args=(matrixA, matrixB, startRow, endRow, queue))
         processes.append(process)
         process.start()
 
     for p in processes:
         p.join()
-     # ----------
+     # --------------------
     matrixResult = [None] * getRowLength(matrixA)
     for workerIndex in range(numberOfProcesses):
         startRow, partial = queue.get()
 
         for index, row in enumerate(partial):
             matrixResult[startRow + index] = row
-     # ----------
-    return matrixResult
+     # --------------------
+    endTime = time.time()         # TIME ENDED
+
+    return {
+        "matrix" : matrixResult,
+        "startTime" : startTime,
+        "endTime" : endTime,
+        "processingTime" : endTime - startTime
+    }
 
 def P1(matrixA, matrixB):
     startTime = time.time()         # TIME STARTED
@@ -73,7 +81,6 @@ def P1(matrixA, matrixB):
 
 def P2(matrixA, matrixB):
     workers = os.cpu_count()
-    startTime = time.time()         # TIME STARTED
 
     matrix = runParallel(
         matrixA,
@@ -81,38 +88,22 @@ def P2(matrixA, matrixB):
         workers
     )
 
-    endTime = time.time()         # TIME ENDED
+    return matrix
 
-    return {
-        "matrix" : matrix,
-        "startTime" : startTime,
-        "endTime" : endTime,
-        "processingTime" : endTime - startTime
-    }
+# ====================
 
-# ==========
-
-def main():
-
-    #inputDefaultPath = os.path.expanduser("~")
+def main(variation=None, filename=None):
     inputDefaultPath = str(Path(__file__).resolve().parent) + "\\data\\input\\"
     outputDefaultPath = str(Path(__file__).resolve().parent) + "\\data\\output\\"
 
-    askMatrix = True
+    matrixAName = inputDefaultPath + filename
+    matrixBName = matrixAName
+    if filename is None:
+        matrixAName = gui.openFile(title="Selecione a Matriz A", fileTypes=[("Text Files", "*.txt")], initialPath=inputDefaultPath, ); exitIfNone(matrixA)
+        #matrixBName = gui.openFile(title="Selecione a Matriz B", fileTypes=[("Text Files", "*.txt")]); exitIfNone(matrixA)\
 
-    if askMatrix:
-        #matrixA = gui.openFile(title="Selecione a Matriz A", fileTypes=[("Text Files", "*.txt")], initialPath=inputDefaultPath, ); exitIfNone(matrixA)
-        #matrixB = gui.openFile(title="Selecione a Matriz B", fileTypes=[("Text Files", "*.txt")]); exitIfNone(matrixA)
-
-        matrixA = inputDefaultPath + "4_int.txt"
-        matrixA = inputDefaultPath + "10_int.txt"
-        #matrixA = inputDefaultPath + "10_float.txt"
-        matrixA = inputDefaultPath + "128.txt"
-        matrixA = inputDefaultPath + "512.txt"
-        matrixA = inputDefaultPath + "1024.txt"
-
-    matrixA = readMatrix(matrixA)
-    matrixB = matrixA
+    matrixA = readMatrix(matrixAName)
+    matrixB = readMatrix(matrixBName)
 
     if (getColumnLength(matrixA)) != (getRowLength(matrixB)):
         gui.showError("O número de colunas da 'matriz A' deve ser o mesmo que o número de linhas da 'matriz B'")
@@ -128,37 +119,32 @@ def main():
         printMatrix(matrixB)
         print("-" * 50)
 
-    #variation = gui.showSelectOptions(["P1", "P2", "P3", "P4", "P5"], title="Selecione", message="Selecione a Variação desejada:")
-    variation = "P2" # =================================
+    if variation is None:
+        variation = gui.showSelectOptions(["P1", "P2", "P3", "P4", "P5"], title="Selecione", message="Selecione a Variação desejada:")
 
     cores = os.cpu_count()
     remoteComputers=0
 
-    # ==========
+    # ====================
     print("STARTING MULTIPLICATION...")
     # ----------
     if variation == "P1":
         matrixResult = P1(matrixA, matrixB)
     # ----------
-    elif  variation == "P2":
-        matrixResult = P2(matrixA, matrixB)
-    elif variation in "P2P3P4" and False:
+    elif variation in "P2 P3 P4":
         workers = cores
         if variation == "P3": workers = cores * 2
         if variation == "P4": workers = max(1, cores // 2)
         
-        
-        endTime = time.time()           # TIME FINISHED
+        matrixResult = runParallel(matrixA, matrixB, workers)
     # ----------
-            
-
-    # ----------
+    
 
     print("MULTIPLICATION FINISHED!")
 
-    # ==========
+    # ====================
 
-    filename = f"Matrix {datetime.now()}.txt".replace(":", "-")  # datetime.now().strftime("Matrix_%Y-%m-%d_%H-%M-%S.txt")
+    filename = f"Matrix {datetime.now()} ({variation} {filename}).txt".replace(":", "-")  # datetime.now().strftime("Matrix_%Y-%m-%d_%H-%M-%S.txt")
 
     print(f"SAVING FILE: {filename}")
     writeMatrixResult(
@@ -176,4 +162,20 @@ def main():
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
-    main()
+    variations = ["P1", "P2", "P3", "P4", "P5"]
+    filenames = [
+        "4_int.txt", 
+        "4_int1.txt",
+        "4_int2.txt",
+        "10_int.txt",
+        "10_float.txt",
+        "128.txt",
+        "512.txt",
+        "1024.txt",
+        "2048.txt"
+        ]
+    
+    for variation in variations[0:4]:
+        for filename in filenames[0:5]:
+            main(variation=variation, filename=filename)
+    gui.showMessage("PROGRAM FINISHED")
